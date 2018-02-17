efd5fb4c110b35ac0b77e7f45951cb0cd610ac8c
Don Kim <dgkimdev@gmail.com>
2018-02-17 13:13:36 +0900

    Add a write protocol, Part 3
    Add the second half of
    https://doc.akka.io/docs/akka/current/guide/tutorial_3.html

--- tutorial/src/main/scala/tutorial/Device.scala
+++ tutorial/src/main/scala/tutorial/Device.scala
@@ -6,6 +6,9 @@ object Device {
   def props(groupId: String, deviceId: String): Props =
     Props(new Device(groupId, deviceId))
 
+  final case class RecordTemperature(requestId: Long, value: Double)
+  final case class TemperatureRecorded(requestId: Long)
+
   final case class ReadTemperature(requestId: Long)
   final case class RespondTemperature(requestId: Long, value: Option[Double])
 }
@@ -23,6 +26,11 @@ with ActorLogging {
     log.info("Device actor {}-{} stopped", groupId, deviceId)
 
   override def receive: Receive = {
+    case RecordTemperature(id, value) =>
+      log.info("Recorded temperature reading {} with {}", value, id)
+      lastTemperatureReading = Some(value)
+      sender() ! TemperatureRecorded(id)
+
     case ReadTemperature(id) =>
       sender() ! RespondTemperature(id, lastTemperatureReading)
   }
